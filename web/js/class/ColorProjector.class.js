e93d774eca8ef0507735d0742d9220e2549a424d
fnagel <elfixlange@gmail.com>
2017-08-09 00:06:27 +0200

    -

--- web/js/class/ColorProjector.class.js
+++ web/js/class/ColorProjector.class.js
@@ -45,9 +45,6 @@ ColorProjector.prototype = {
 	iBMax: 255,
 
 	init: function init(){
-		this.iAR = ((this.iAlpha)%360)/180*Math.PI;
-		this.iAG = ((this.iAlpha+120)%360)/180*Math.PI;
-		this.iAB = ((this.iAlpha+240)%360)/180*Math.PI;
 
 		/*
 		this.iXR = this.iR*Math.cos((this.iAlpha%360)/180*Math.PI);
@@ -62,21 +59,25 @@ ColorProjector.prototype = {
 	},
 
 	calc_rads: function calc_rads(){
+		this.iAlpha += 0.1;
+		this.iAR = ((this.iAlpha)%360)/180*Math.PI;
+		this.iAG = ((this.iAlpha+120)%360)/180*Math.PI;
+		this.iAB = ((this.iAlpha+240)%360)/180*Math.PI;
 	
 	},
 
 	calc_dist: function calc_dist(iX, iY){
 		this.iDist = Math.sqrt(Math.pow(iX, 2)+Math.pow(iY, 2));
 		this.iPhi = Math.atan2(iY, iX);
-		this.iFR = Math.cos(this.iPhi*2/3-this.iAR);
+		this.iFR = Math.cos((this.iPhi-this.iAR)*3/4);
 		if(this.iFR < 0){
 			this.iFR = 0;
 		}
-		this.iFG = Math.cos(this.iPhi*2/3-this.iAG);
+		this.iFG = Math.cos((this.iPhi-this.iAG)*3/4);
 		if(this.iFG < 0){
 			this.iFG = 0;
 		}
-		this.iFB = Math.cos(this.iPhi*2/3-this.iAB);
+		this.iFB = Math.cos((this.iPhi-this.iAB)*3/4);
 		if(this.iFB < 0){
 			this.iFB = 0;
 		}
@@ -89,13 +90,70 @@ ColorProjector.prototype = {
 	},
 
 	get_rgb: function get_rgb(iX, iY){
-
+		this.calc_rads();
 		this.calc_dist(iX, iY);
 		this.calc_rgb();
-		console.log(this.iRCurr+','+this.iGCurr+','+this.iBCurr);
+		//console.log(this.iRCurr+','+this.iGCurr+','+this.iBCurr);
 		return this.iRCurr+','+this.iGCurr+','+this.iBCurr;
 		/*
 		console.log(this.iRCurr+','+this.iGCurr+','+this.iBCurr);
 		*/
 	}
+
+/*
+<div id="test" style="width:200px; height:200px; border:1px solid red; position:relative">
+	<div style="position:absolute; top:99px; left:99px; right:99px; bottom:99px; background-color:red"></div>
+</div>
+<div id="test-color" style="margin-top:10px; height:500px; width:100%; border:1px solid red;">
+	<p id="test-color2" style="font-size:400px; text-shadow:10px 10px 0px rgba(0, 0, 0, 0.2);">FOOBAR FOOBAR</p>
+</div>
+{literal}<script type="text/javascript">
+	$(document).ready(function(){ 'use strict';
+		var $w = $('#test');
+		var $testColor = $('#test-color');
+		var $testColor2 = $('#test-color2');
+		var rotX = function rotX(x, y, alpha){
+			return x*Math.cos(alpha) - y*Math.sin(alpha)
+		};
+		var rotY = function rotY(x, y, alpha){
+			return x*Math.sin(alpha) + y*Math.cos(alpha);
+		};
+		var getF = function getF(x, y, alpha){
+			var phi = Math.atan2(rotY(x, y, alpha), rotX(x, y, alpha));
+			var f = 0;
+			if(phi < 2/3*Math.PI && phi > -2/3*Math.PI){
+				f = Math.cos(phi*3/4);
+			}
+			f *= Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+			return f;
+		};
+		var getR = function getR(x, y){
+			var alpha = 0*Math.PI;
+			//return 255*getF(x, y, alpha)>>0;
+			return 255-255*getF(x, y, alpha)>>0;
+		};
+		var getG = function getG(x, y){
+			var alpha = -2/3*Math.PI;
+			//return 255*getF(x, y, alpha)>>0;
+			return 255-255*getF(x, y, alpha)>>0;
+		};
+		var getB = function getB(x, y){
+			var alpha = -4/3*Math.PI;
+			//return 255*getF(x, y, alpha)>>0;
+			return 255-255*getF(x, y, alpha)>>0;
+		};
+		$w.on('mousemove', function(event){
+			var x = (event.offsetX - 100) / 100;
+			var y = (event.offsetY - 100) / 100;
+			$testColor[0].style.backgroundColor = 'rgb('+getR(x, y)+','+getG(x, y)+','+getB(x, y)+')';
+			$testColor2[0].style.color = 'rgb('+getR(x, y)+','+getG(x, y)+','+getB(x, y)+')';
+			$testColor2[0].style.textShadow = 20*rotX(x, y, Math.PI)+'px '+20*rotY(x, y, Math.PI)+'px 2px rgba(0, 0, 0, 0.3)';
+			$testColor2[0].style.transform = 'rotateX('+y*-15+'deg)' + ' ' + 'rotateY('+x*15+'deg)';
+			
+		});
+	});
+</script>{/literal}
+*/
+
+
 };
